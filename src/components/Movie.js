3dd1d91e82526927e13b8434086527f7cfe5c705
MPalso <martin.palsovic@live.com>
2023-10-23 19:53:30 +0200

    Pridanie AllDeleteButton - vymazat vsetko

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,6 +2,7 @@ import "./Movie.css"
 import data from "../data"
 import MovieDeleteButton from "./MovieDeleteButton"
 import { useState } from "react"
+import AllDeleteButton from "./AllDeleteButton"
 
 const Movie = () => {
     const [movieList, setMovieList] = useState(data)
@@ -13,24 +14,33 @@ const Movie = () => {
         setMovieList(filterMovies)
     }
 
+    const deleteAllMovies = () => {
+        setMovieList([])
+    }
+
 
     return  (
-        <div className="all-movies">
-            {
-                movieList.map( (oneMovie) => {
-                    const {id, image, title, age, tags, description} = oneMovie
-                
-                    return <div className="one-movie" key={id}>
-                             <img src={image} alt="" />
-                             <h2>{title}</h2>
-                            <p>{age}</p>
-                            <p>{tags}</p>
-                            <p>{description}</p>
-                            <MovieDeleteButton deleteMovie={() => deleteOneMovie(id)} />
-                    </div>
-                })
-            }
-        </div>
+        <section>
+            <div className="all-movies">
+                {
+                    movieList.map( (oneMovie) => {
+                        const {id, image, title, age, tags, description} = oneMovie
+                    
+                        return <div className="one-movie" key={id}>
+                                <img src={image} alt="" />
+                                <h2>{title}</h2>
+                                <p>{age}</p>
+                                <p>{tags}</p>
+                                <p>{description}</p>
+                                <MovieDeleteButton deleteMovie={() => deleteOneMovie(id)} />
+                        </div>
+                    })
+                }
+            </div>
+            <div>
+                <AllDeleteButton deleteMovies ={deleteAllMovies}/>
+            </div>
+        </section>
     )
 
 }
